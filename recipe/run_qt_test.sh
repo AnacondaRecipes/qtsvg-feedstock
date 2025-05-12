9abb068f6f9306e1f4fdd76988cb1801091e431d
Serhii Kupriienko <skupriienko@anaconda.com>
2025-05-12 16:56:05 +0300

    Automated rebuild for x11_gui_rebuilds_repair

--- recipe/run_qt_test.sh
+++ recipe/run_qt_test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-set -ex
+set -e
 
+echo "=== Qt SVG Test Starting ==="
+
+# Set library paths for conda cross-compilation
 export LD_LIBRARY_PATH="${PREFIX}/${HOST}/sysroot/usr/lib64:${PREFIX}/${HOST}/sysroot/usr/lib:${LD_LIBRARY_PATH}"
 
-test -f ${PREFIX}/bin/qt6.conf
-
+# Configure and build test
 cmake -Stest -Bbuild -GNinja
+
 cmake --build build --target all
+
+# Run tests
 ctest --test-dir build --output-on-failure
